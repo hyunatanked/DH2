0b2a6a2471967fe341dd6c2b1f29fb598f44a40d
hyunatanked <dbruton2009@gmail.com>
2025-04-22 13:14:08 -0400

    cookie run data

---
+++ data/mods/cookiemon/formats-data.ts
@@ -0,0 +1,62 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	shadowmilkcookie: {
+		tier: "OU",
+	},
+	mysticflowercookie: {
+		tier: "OU",
+	},
+	burningspicecookie: {
+		tier: "OU",
+	},
+	windarchercookie: {
+		tier: "OU",
+	},
+	firespiritcookie: {
+		tier: "OU",
+	},
+	seafairycookie: {
+		tier: "OU",
+	},
+	cottoncookie: {
+		tier: "OU",
+	},
+	goldenosmanthuscookie: {
+		tier: "OU",
+	},
+	rebelcookie: {
+		tier: "OU",
+	},
+	darkcacaocookie: {
+		tier: "OU",
+	},
+	goldencheesecookie: {
+		tier: "OU",
+	},
+	hollyberrycookie: {
+		tier: "OU",
+	},
+	whitelilycookie: {
+		tier: "OU",
+	},
+	purevanillacookie: {
+		tier: "OU",
+	}, 
+	milkywaycookie: {
+		tier: "OU",
+	}, 
+	frilledjellyfishcookie: {
+		tier: "OU",
+	}, 
+	stormbringercookie: {
+		tier: "OU",
+	}, 
+	blackpearlcookie: {
+		tier: "OU",
+	}, 
+	frostqueencookie: {
+		tier: "OU",
+	}, 
+	moonlightcookie: {
+		tier: "OU",
+	}, 
+};
